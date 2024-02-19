639704fef77b16406282b5e0343296a1fe26d896
Adrian <adrianginergimenez@gmail.com>
2024-02-19 13:53:50 +0100

    Prueba de commit desde VSC

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
- // Aquí tu código
+// Aquí tu código
+const addElementBtn = document.getElementById('agregar');
+const listParent = document.getElementById('lista');
+
+const ewew = prompt('Introduce un nuevo elemento');
+
+addElementBtn.addEventListener('click', function() {
+    const newElement = prompt('Introduce un nuevo elemento');
+    console.log('Paco');
+    //listParent.insertAdjacentHTML("afterbegin", `<li>${newElement}</li>`);
+});
+
